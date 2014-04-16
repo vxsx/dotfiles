972863b58284d17a5f138eec9aaa6d1442536f4f
Vadim Sikora <vadim.sikora@gmail.com>
2014-04-16 23:21:05 +0200

    rm unnecessary

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,5 @@
 # Aliases
 alias memcache='memcached -m 256 -p 11211 -l 127.0.0.1 -d -u www && say memcached started'
-alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
 alias ssh='TERM=screen ssh'
 alias o='open'
 alias oo='open .'
