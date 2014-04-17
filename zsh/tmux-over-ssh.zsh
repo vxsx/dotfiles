c882497370d6f74c7b993ea286634cd38564d2bb
Vadim Sikora <vadim.sikora@gmail.com>
2014-04-17 12:38:14 +0200

    added ssh forwarding solution for tmux/screen

---
+++ zsh/tmux-over-ssh.zsh
@@ -0,0 +1,11 @@
+# predictable place for ssh socket, so
+# you can detach tmux on remote server,
+# then come back and ssh from there would still work
+
+SOCK="/tmp/ssh-agent-$USER-screen"
+if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $SOCK ]
+then
+    rm -f /tmp/ssh-agent-$USER-screen
+    ln -sf $SSH_AUTH_SOCK $SOCK
+    export SSH_AUTH_SOCK=$SOCK
+fi
