6fa651c43c30cfeab4adba234d67dd13a6e61d8e
Vadim Sikora <vadim.sikora@gmail.com>
2014-04-18 10:44:24 +0200

    added aliases for showing/hiding hidden files on osx

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,5 @@ alias memcache='memcached -m 256 -p 11211 -l 127.0.0.1 -d -u www && say memcache
 alias ssh='TERM=screen ssh'
 alias o='open'
 alias oo='open .'
+alias showHiddenFiles="defaults write com.apple.finder AppleShowAllFiles TRUE && killall Finder"
+alias hideHiddenFiles="defaults write com.apple.finder AppleShowAllFiles FALSE && killall Finder"
