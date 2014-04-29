2d251038229d160b3ad99780e2fc4b8913a8ef81
Vadim Sikora <vadim.sikora@gmail.com>
2014-04-29 10:36:03 +0200

    add useful "update" alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,5 +3,10 @@ alias memcache='memcached -m 256 -p 11211 -l 127.0.0.1 -d -u www && say memcache
 alias ssh='TERM=screen ssh'
 alias o='open'
 alias oo='open .'
+
+# Show/hide hidden files from finder
 alias showHiddenFiles="defaults write com.apple.finder AppleShowAllFiles TRUE && killall Finder"
 alias hideHiddenFiles="defaults write com.apple.finder AppleShowAllFiles FALSE && killall Finder"
+
+# Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
+alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm update npm -g; npm update -g; sudo gem update --system; sudo gem update'
