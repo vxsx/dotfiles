f7f50858f8b0dd2a222865196798c326078d926e
Vadim Sikora <vadim.sikora@gmail.com>
2014-10-26 18:14:34 +0100

    photoshop alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,3 +10,4 @@ alias hideHiddenFiles="defaults write com.apple.finder AppleShowAllFiles FALSE &
 
 # Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
 alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm update npm -g; npm update -g; sudo gem update --system; sudo gem update'
+alias photoshop='open -b "com.adobe.Photoshop"'
